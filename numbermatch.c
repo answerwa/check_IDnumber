de11162a144e98a18e2ccb863a7bf4a8e12eeb1c
answerwa <answerwa@126.com>
2015-04-17 16:58:29 +0800

    add check area

--- numbermatch.c
+++ numbermatch.c
@@ -11,6 +11,19 @@ void numbermatch(const char *number)
 	int iNumber[] = {7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2};
 	char lastChar[] = {'1','0','X','9','8','7','6','5','4','3','2'}; 
 
+	char area[] = {
+		0,0,0,0,0,0,0,0,0,0,
+		0,1,1,1,1,1,0,0,0,0,
+		0,1,1,1,0,0,0,0,0,0,
+		0,1,1,1,1,1,1,1,0,0,
+		0,1,1,1,1,1,1,0,0,0,
+		1,1,1,1,1,0,0,0,0,0,
+		0,1,1,1,1,1,0,0,0,0,
+		0,1,0,0,0,0,0,0,0,0,
+		0,1,1,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0
+	};
+
 	regex_t reg;
 	const size_t nmatch = 1;
 	regmatch_t pmatch[1];
@@ -21,8 +34,11 @@ void numbermatch(const char *number)
 	
 	int year, month, day;
 
+	int area_number;
+#ifdef YEAR
 	time_t now;
 	struct tm *tm;
+#endif
 
 
 	status = regcomp(&reg, PATTERN, REG_EXTENDED|REG_ICASE);
@@ -35,46 +51,53 @@ void numbermatch(const char *number)
 	if (status == REG_NOMATCH) {
 		printf("No Match \n");
 	} else if(status == 0) {
-#ifdef YEAR
-		//check year
-		time(&now);
-		tm = localtime(&now);
-
-		year = (number[pmatch[0].rm_so+6]-48)*1000 + (number[pmatch[0].rm_so+7]-48)*100 + (number[pmatch[0].rm_so+8]-48)*10 + (number[pmatch[0].rm_so+9]-48);
-		if(year > (tm->tm_year+1900) || tm->tm_year+1900-year > 150) {
-			printf("birth year is %d, wrong! offset is %d\n", year, offset_store + pmatch[0].rm_so);
+		// check area
+		area_number = (number[pmatch[0].rm_so]-48)* 10 + (number[pmatch[0].rm_so+1]-48);
+		if(area[area_number] == 0) {
+			printf("area is %d, wrong! offset is %d\n", area_number, offset_store + pmatch[0].rm_so);
 			offset_store += pmatch[0].rm_so+1;
 			numbermatch(number+pmatch[0].rm_so+1);
 		} else {
-#endif
-			// check month
-			month = (number[pmatch[0].rm_so+10]-48)* 10 + (number[pmatch[0].rm_so+11]-48);
-			if(month > 12 || month == 0) {
-				printf("month is %d, wrong! offset is %d\n", month, offset_store + pmatch[0].rm_so);
+#ifdef YEAR
+			//check year
+			time(&now);
+			tm = localtime(&now);
+	
+			year = (number[pmatch[0].rm_so+6]-48)*1000 + (number[pmatch[0].rm_so+7]-48)*100 + (number[pmatch[0].rm_so+8]-48)*10 + (number[pmatch[0].rm_so+9]-48);
+			if(year > (tm->tm_year+1900) || tm->tm_year+1900-year > 150) {
+				printf("birth year is %d, wrong! offset is %d\n", year, offset_store + pmatch[0].rm_so);
 				offset_store += pmatch[0].rm_so+1;
 				numbermatch(number+pmatch[0].rm_so+1);
 			} else {
-				//check day
-				day = (number[pmatch[0].rm_so+12]-48) * 10 + (number[pmatch[0].rm_so+13]-48);
-				if(day > 31 || day == 0) {
-					printf("day is %d, wrong! offset is %d\n", day, offset_store + pmatch[0].rm_so);
+#endif
+				// check month
+				month = (number[pmatch[0].rm_so+10]-48)*10 + (number[pmatch[0].rm_so+11]-48);
+				if(month > 12 || month == 0) {
+					printf("month is %d, wrong! offset is %d\n", month, offset_store + pmatch[0].rm_so);
 					offset_store += pmatch[0].rm_so+1;
 					numbermatch(number+pmatch[0].rm_so+1);
 				} else {
-					// check last number
-					for(i=pmatch[0].rm_so, j=0; j<17; ++i,++j)
-						sum += (number[i] - 48) * iNumber[j];
-					if(lastChar[sum%11] != number[i]) {
-						printf("last number should be %c! offset is %d\n", lastChar[sum%11], offset_store + pmatch[0].rm_so);
+					//check day
+					day = (number[pmatch[0].rm_so+12]-48)*10 + (number[pmatch[0].rm_so+13]-48);
+					if(day > 31 || day == 0) {
+						printf("day is %d, wrong! offset is %d\n", day, offset_store + pmatch[0].rm_so);
 						offset_store += pmatch[0].rm_so+1;
 						numbermatch(number+pmatch[0].rm_so+1);
 					} else {
-						// check over
-						printf("***this is a ID number! offset is %d***\n", offset_store + pmatch[0].rm_so);
-						offset_store += pmatch[0].rm_eo;
-						numbermatch(number+pmatch[0].rm_eo);
+						// check last number
+						for(i=pmatch[0].rm_so, j=0; j<17; ++i,++j)
+							sum += (number[i] - 48) * iNumber[j];
+						if(lastChar[sum%11] != number[i]) {
+							printf("last number should be %c! offset is %d\n", lastChar[sum%11], offset_store + pmatch[0].rm_so);
+							offset_store += pmatch[0].rm_so+1;
+							numbermatch(number+pmatch[0].rm_so+1);
+						} else {
+							// check over
+							printf("***this is a ID number! offset is %d***\n", offset_store + pmatch[0].rm_so);
+							offset_store += pmatch[0].rm_eo;
+							numbermatch(number+pmatch[0].rm_eo);
+						}
 					}
-
 				}
 			}
 #ifdef YEAR
